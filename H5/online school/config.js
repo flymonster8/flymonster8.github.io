0c8fbc4ed70f6d36b6d1de6dd65b8ba52dfefb80
ayc <ww921224@163.com>
2018-04-17 21:49:23 +0800

    add online school

---
+++ H5/online school/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name : 'OLS',
+    devPath : 'D:\online school', //项目根路径，根路径下可以包含多个项目
+    prodPath : 'D:\online school/dist', //生产路径根路径
+    sassPath : 'D:\online school/sass', //SASS包含文件路径
+    rmHtmlWhitespace : false,//html中是否去除空格
+    server : {
+        port : 8088
+    }
+};
